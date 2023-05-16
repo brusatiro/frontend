0d12ae167a41adcf208c1b564e05bf52ae1d87c1
Bruna Satiro <brunasatiro@outlook.com>
2023-05-15 23:16:11 -0300

    deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -12,7 +12,7 @@ import axios from 'axios'
 
 
 const api = axios.create({
-    baseURL: "https://backend-fiap.herokuapp.com/tasks"
+    baseURL: "https://backend-fiap.herokuapp.com"
     })
 
 export default api;
